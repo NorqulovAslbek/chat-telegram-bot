c1c4b7457f7a2d096a143e7ac62c9177d8ff59ca
Salim <juratergashev580@gmail.com>
2024-12-09 17:08:50 +0500

    Committed

--- src/main/kotlin/com/example/chattelegrambot/ChatTelegramBotApplication.kt
+++ src/main/kotlin/com/example/chattelegrambot/ChatTelegramBotApplication.kt
@@ -2,8 +2,12 @@ package com.example.chattelegrambot
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableJpaRepositories(repositoryBaseClass = BaseRepositoryImpl::class)
 class ChatTelegramBotApplication
 
 fun main(args: Array<String>) {
